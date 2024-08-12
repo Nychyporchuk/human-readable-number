32de88b3135f4528aa28374e778e322047bd149d
Nychyporchuk <bloodever@gmail.com>
2024-08-12 14:25:37 +0300

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    const upToTwenty = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ];
+
+    const tens = [
+        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+
+    if (number < 20) {
+        return upToTwenty[number];
+    } else if (number < 100) {
+        return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + upToTwenty[number % 10] : "");
+    } else if (number < 1000) {
+        return upToTwenty[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+    } else {
+        return "number not defined";
+    }
 }
